acd1a1a1351404087d30183ba98774879a8afaab
MarkieAurelius <markieaurelius@gmail.com>
2025-06-16 14:31:00 -0700

    Saving because trying doom emacs for faster startup time

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,24 @@
+# ~/.config/fish/config.fish
+
+# Define custom prompt
+function fish_prompt
+    echo -n '['(prompt_pwd)']$ '
+end
+
 if status is-interactive
+    # Disable fish greeting
     set fish_greeting
 
-    # Commands to run in interactive sessions can go here
+    # Aliases
     alias cat='bat'
     alias ls='eza --icons --time-style=long-iso --group-directories-first'
+    alias vim='nvim'
+
+    # zoxide and other initializations
     zoxide init fish --cmd cd | source
     # starship init fish | source
 
+    # Editor variables (universal or global)
     set -Ux EDITOR nvim
     set -Ux VISUAL nvim
 end
--- nvim/lua/markie/lazy/obsidian.lua
+++ nvim/lua/markie/lazy/obsidian.lua
@@ -1,34 +1,49 @@
 return {
   "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
+  version = "*",
   lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
-    -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
     workspaces = {
       {
         name = "personal",
-        path = "~/vaults/personal",
-      },
-      {
-        name = "work",
-        path = "~/vaults/work",
+        path = "~/notes",
       },
     },
-
-    -- see below for full list of options 👇
+    daily_notes = {
+      folder = "daily",
+      date_format = "%Y-%m-%d",
+      template = nil,
+    },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+    disable_frontmatter = false,
+    templates = {
+      folder = "templates",
+    },
+    picker = {
+      name = "telescope.nvim",
+      note_mappings = {
+        new = "<C-x>",
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        tag_note = "<C-x>",
+        insert_tag = "<C-l>",
+      },
+    },
   },
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = function()
+        vim.opt_local.conceallevel = 2
+      end,
+    })
+  end,
 }
