faa318a633e0ec84dbdd6487286d0428eb551fdf
Mark <MarkieAurelius@gmail.com>
2025-06-30 18:14:39 -0700

    Update config.el

--- doom/config.el
+++ doom/config.el
@@ -1,1 +1,9 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+(setq doom-font (font-spec :family "MesloLGM Nerd Font Mono" :size 24))
+
+(setq doom-theme 'doom-one-light)
+
+(setq display-line-numbers-type 'relative)
+
+(setq org-directory "~/notes/")
