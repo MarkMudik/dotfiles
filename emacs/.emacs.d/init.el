fa80451eac4a7b829b06b698f72e5f0373b7d032
MarkMudik <MarkieAurelius@gmail.com>
2025-07-26 23:30:17 -0700

    emacs stuff

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -293,27 +293,27 @@
   :hook (after-init . spacious-padding-mode))
 
 ;;; mu4e
-(use-package mu4e
-  :ensure nil
-  :load-path "/usr/share/emacs/site-lisp/mu4e"
-  ;; :defer 20 ; uncomment after a bit of time
-  :config
-  (setq mu4e-change-filenames-when-moving t)
-  (setq mu4e-update-interval (* 30 60))
-  (setq mu4e-get-mail-command "mbsync -a")
-  (setq mu4e-maildir "~/.mail/personal")
+;(use-package mu4e
+;  :ensure nil
+;  :load-path "/usr/share/emacs/site-lisp/mu4e"
+;  ;; :defer 20 ; uncomment after a bit of time
+;  :config
+;  (setq mu4e-change-filenames-when-moving t)
+;  (setq mu4e-update-interval (* 30 60))
+;  (setq mu4e-get-mail-command "mbsync -a")
+;  (setq mu4e-maildir "~/.mail/personal")
 
-  (setq mu4e-drafts-folder "[Gmail]/Drafts")
-  (setq mu4e-sent-folder "[Gmail]/Sent Mail")
-  (setq mu4e-refile-folder "[Gmail]/All Mail")
-  (setq mu4e-trash-folder "[Gmail]/Trash")
+  ;(setq mu4e-drafts-folder "[Gmail]/Drafts")
+  ;(setq mu4e-sent-folder "[Gmail]/Sent Mail")
+  ;(setq mu4e-refile-folder "[Gmail]/All Mail")
+  ;(setq mu4e-trash-folder "[Gmail]/Trash")
 
-  (setq mu4e-maildir-shortcuts
-        '((:maildir "Inbox"    :key ?i)
-          (:maildir "[Gmail]/Sent Mail" :key ?s)
-          (:maildir "[Gmail]/Trash"     :key ?t)
-          (:maildir "[Gmail]/Drafts"    :key ?d)
-          (:maildir "[Gmail]/All Mail"  :key ?a))))
+  ;(setq mu4e-maildir-shortcuts
+  ;      '((:maildir "Inbox"    :key ?i)
+  ;        (:maildir "[Gmail]/Sent Mail" :key ?s)
+  ;        (:maildir "[Gmail]/Trash"     :key ?t)
+  ;        (:maildir "[Gmail]/Drafts"    :key ?d)
+  ;        (:maildir "[Gmail]/All Mail"  :key ?a))))
 
 (use-package markdown-mode
   :ensure t
